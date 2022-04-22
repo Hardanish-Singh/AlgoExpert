e6d0f2f2816583a710a36981eaf8db99be00b6f2
Hardanish Singh <hardanishsingh1@gmail.com>
2022-04-22 09:22:56 -0400

    Tadem Bicycle

--- Greedy_Algorithms/Tadem_Bicycle.js
+++ Greedy_Algorithms/Tadem_Bicycle.js
@@ -19,11 +19,9 @@ function tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest) {
         redShirtSpeeds.sort( ( a, b ) => a - b );
         blueShirtSpeeds.sort( ( a, b ) => a - b );
         
-        var j = blueShirtSpeeds.length - 1;
         var sum = 0;      
         for( let i = 0; i < redShirtSpeeds.length; i++ ) {
-                fastest === true ? sum += Math.max( redShirtSpeeds[i], blueShirtSpeeds[j] ) : sum += Math.max( redShirtSpeeds[i], blueShirtSpeeds[i] );
-                j--;
+                fastest === true ? sum += Math.max( redShirtSpeeds[ i ], blueShirtSpeeds[ blueShirtSpeeds[ blueShirtSpeeds.length - 1 - i ] ] ) : sum += Math.max( redShirtSpeeds[ i ], blueShirtSpeeds[ i ] );
         }
               
         return sum;
