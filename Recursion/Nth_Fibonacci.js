ee7f79282c93f61a64698100dbc759d0bd4eab50
Hardanish Singh <hardanishsingh1@gmail.com>
2022-04-25 09:04:29 -0400

    Nth Fibonacci

---
+++ Recursion/Nth_Fibonacci.js
@@ -0,0 +1,24 @@
+function getNthFib(n) {
+        if( n == 1 ) {
+                return 0;
+        }
+        if( n == 2 ) {
+                return 1;
+        }
+        var prev = 0;
+        var current = 1;
+        var count = 2;
+        var next = 0;
+        while( count < n ) {
+                next = prev + current;
+                prev = current;
+                current = next;
+                count++;
+        }
+        return next;
+}
+      
+// Do not edit the line below.
+exports.getNthFib = getNthFib;
+      
+      
