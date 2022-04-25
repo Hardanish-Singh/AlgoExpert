801445b534923f7602113270e1c519d4ad2a86a6
Hardanish Singh <hardanishsingh1@gmail.com>
2022-04-25 11:49:26 -0400

    Powerset

--- Recursion/Powerset.js
+++ Recursion/Powerset.js
@@ -1,3 +1,17 @@
+/****************************************************************
+*                       ALGOEXPERT POWERSET CHALLENGE	        *
+*                                                               *
+* Write a function that takes in an array of unique integers    *
+* and returns its powerset. The powerset P(X) of a set X is the *
+* set of all subsets of X.                                      *
+* For example, the powerset of [1,2] is  [[], [1], [2], [1,2]]. *
+* The sets in the powerset can be in any particular order.      *
+*                                                               *
+* Input 1: [1, 2, 3]                                            *
+* Output 1: [[1],[2],[3],[1, 2],[1, 3],[2, 3],[1, 2, 3],[]]     *
+*                                                               *
+****************************************************************/
+
 function powerset(array) {
         if( array.length === 0 ) {
                 return [ [] ];
