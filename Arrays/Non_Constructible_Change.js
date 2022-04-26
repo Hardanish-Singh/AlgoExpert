d3c56e4813a5162aba0830b9b910cc7137a3a589
Hardanish Singh <hardanishsingh1@gmail.com>
2022-04-26 15:48:59 -0400

    Non Constructible Change

--- Arrays/Non_Constructible_Change.js
+++ Arrays/Non_Constructible_Change.js
@@ -1,3 +1,23 @@
+/****************************************************************
+*            ALGOEXPERT NON CONSTRUCTIBLE CHANGE CHALLENGE	*
+*                                                               *
+* Given an array of positive integers representing the values of*
+* coins in your possession, write a function that returns the   *
+* minimum amount of change (the minimum sum of money) that you  *
+* cannot create. The given coins can have any positive integer  *
+* value and aren't necessarily unique (i.e., you can have       *
+* multiple coins of the same value).                            *
+*                                                               *
+* For example, if you're given coins = [1, 2, 5], the minimum   *
+* amount of change that you can't create is 4.                  *
+* If you're given no coins, the minimum amount of change that   *
+* you can't create is 1.                                        *
+*                                                               *
+* Input 1: [5, 7, 1, 1, 2, 3, 22]                               *
+* Output 1: 20                                                  *
+*                                                               *
+****************************************************************/
+
 function nonConstructibleChange(array) {
         if(array.length === 0 ) {
                 return 1;
