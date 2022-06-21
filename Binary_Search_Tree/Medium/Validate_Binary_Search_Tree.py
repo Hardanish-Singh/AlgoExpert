21459117eea37bffc9af9338770e02cc6d38e6d3
Hardanish Singh <hardanishsingh1@gmail.com>
2022-06-21 11:30:07 -0400

    Validate Binary Search Tree

--- Binary_Search_Tree/Medium/Validate_Binary_Search_Tree.py
+++ Binary_Search_Tree/Medium/Validate_Binary_Search_Tree.py
@@ -1,4 +1,14 @@
 # This is an input class. Do not edit.
+"""
+        Write a function that takes in a potentially invalid Binary Search Tree (BST) and returns a boolean representing whether the BST is valid.
+        Each BST node has an integer value, a left child node, and a right child node. 
+        A node is said to be a valid BST node if and only if it satisfies the BST property: 
+                1) its value is strictly greater than the values of every node to its left; 
+                2) its value is less than or equal to the values of every node to its right; 
+                3) its children nodes are either valid  BST nodes themselves or None / null.
+        A BST is valid if and only if all of its nodes are valid  BST nodes.
+"""
+
 class BST:
         def __init__(self, value):
                 self.value = value
