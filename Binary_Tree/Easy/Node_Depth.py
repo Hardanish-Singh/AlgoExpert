d7502e541706918ff401bc8f53e57934bba1c680
Hardanish Singh <hardanishsingh1@gmail.com>
2022-06-28 08:46:25 -0400

    Node Depth

---
+++ Binary_Tree/Easy/Node_Depth.py
@@ -0,0 +1,22 @@
+# This is the class of the input binary tree.
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+def nodeDepths(root, sum = 0):
+    sum = 0
+    stack = [ [ root, 0 ] ]
+
+    while len( stack ) > 0:
+        node, depth = stack.pop()
+        if node is None:
+            continue
+        sum = sum + depth
+        if node.left:
+            stack.append( [ node.left, depth + 1 ] )
+        if node.right:
+            stack.append( [ node.right, depth + 1 ] )
+
+    return sum
