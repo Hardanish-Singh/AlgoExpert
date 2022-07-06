ca8e79025d077c4ef0b5171a4200ef436155b7f5
Hardanish Singh <hardanishsingh1@gmail.com>
2022-07-06 09:12:21 -0400

    Permutation

--- Recursion/Permutation.js
+++ Recursion/Permutation.js
@@ -1,3 +1,17 @@
+/*******************************************************************************
+*                       ALGOEXPERT PERMUTATION CHALLENGE	                *
+*                                                                               *
+* Write a function that takes in an array of unique integers and returns an     *
+* array of all permutations of those integers in no particular order.           *
+*                                                                               *
+* If the input array is empty, the function should return an empty array.       *
+*                                                                               *
+* Input 1:  [1, 2, 3 ]                                                          *
+* Output 1: [ [1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1] ]*
+*                                                                               *
+********************************************************************************/
+
+
 function is_complete( array1, array2 ) {
         let is_complete = true;
         for( let k=0; k<array1.length; k++ ) {
