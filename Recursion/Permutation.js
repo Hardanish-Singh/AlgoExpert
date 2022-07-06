19f57a97b41afd984166bd6c6002a2d9f35c42e6
Hardanish Singh <hardanishsingh1@gmail.com>
2022-07-06 08:17:56 -0400

    Permutation

--- Recursion/Permutation.js
+++ Recursion/Permutation.js
@@ -1,19 +1,3 @@
-/*
-        Write a function that takes in an array of unique integers and returns an array of all permutations of those integers in no particular order.
-        If the input array is empty, the function should return an empty array.
-
-        Sample Input: array = [1, 2, 3]
-        Sample Output:
-                        [
-                                [1, 2, 3], 
-                                [1, 3, 2], 
-                                [2, 1, 3], 
-                                [2, 3, 1], 
-                                [3, 1, 2], 
-                                [3, 2, 1]
-                        ]
-*/
-
 function is_complete( array1, array2 ) {
         let is_complete = true;
         for( let k=0; k<array1.length; k++ ) {
