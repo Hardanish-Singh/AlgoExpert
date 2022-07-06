47a614c6eeeb36722746cb55dd0b4a92336bc57b
Hardanish Singh <hardanishsingh1@gmail.com>
2022-07-06 08:17:18 -0400

    Permutation

--- Recursion/Permutation.js
+++ Recursion/Permutation.js
@@ -14,43 +14,46 @@
                         ]
 */
 
+function is_complete( array1, array2 ) {
+        let is_complete = true;
+        for( let k=0; k<array1.length; k++ ) {
+                if( array1[k].length !== array2.length ) {
+                        is_complete = false;
+                        break;
+                }
+        }
+        return is_complete ? false : true;
+}
+
 function getPermutations( nums ) {
         if( nums.length === 0 ) {
                 return [];
         }
+        if( nums.length === 1 ) {
+                return [nums];
+        }
         let permutations = [];
         let temp = [];
         let temp_permutations = [];
-        for( let i = 0; i < nums.length; i++ ) {
-                temp_permutations.push( [ nums[i] ] );
-                temp = [...nums.slice(0, i), ...nums.slice(i + 1)];
-                
-                while( true ) {
-                        
-                        let is_complete = true;
-                        for( let k = 0; k < temp_permutations.length; k++ ){
-                                if( temp_permutations[k].length !== nums.length ) {
-                                        is_complete = false;
-                                        break;
-                                }
-                        }
-                        if( is_complete ) {
-                                break;
-                        }
-
+        for( let i=0; i<nums.length; i++ ) {
+                temp_permutations.push([nums[i]]);
+                temp = [...nums.slice(0,i), ...nums.slice(i+1)];
+                while( is_complete( temp_permutations, nums ) ) {
                         let elementArray = temp_permutations.shift();
-                        for( let j = 0; j < temp.length; j++ ) {
+                        for( let j=0; j<temp.length; j++ ) {
                                 if( ! ( elementArray.includes( temp[j] ) ) ) {
+                                        let t = [ ...elementArray, temp[j] ];
+                                        if( t.length === nums.length ){
+                                                permutations.push( t );
+                                        }
                                         temp_permutations.push( [ ...elementArray, temp[j] ] );
                                 }
                         }
                 }
-                for( let g = 0; g < temp_permutations.length; g++ ) {
-                        permutations.push( temp_permutations[g] );
-                }
+                temp_permutations = [];
         }
         return permutations;
-}
+};
 
 // Do not edit the line below.
 exports.getPermutations = getPermutations;
