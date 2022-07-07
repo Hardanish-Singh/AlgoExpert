8f5280b3d8d4dfbad42d0271fd38149083638aa4
Hardanish Singh <hardanishsingh1@gmail.com>
2022-07-07 08:11:28 -0400

    First Duplicate Value

--- Arrays/First_Duplicate_Value.js
+++ Arrays/First_Duplicate_Value.js
@@ -3,29 +3,29 @@ function sortDictionaryByValues(dict) {
         items.sort( (first, second) => { return first[1] - second[1] } );
         var keys = items.map( (e) => { return e[0] } );
         return keys;
-    } 
+} 
     
-    function firstDuplicateValue( array ) {
-      let duplicates = {
-        
-      };
+function firstDuplicateValue( array ) {
+        let duplicates = {
+
+        };
       
-      for( let i = 0; i < array.length; i++ ) {
-        if( array[ i ] in duplicates ) {
-          continue;
+        for( let i = 0; i < array.length; i++ ) {
+                if( array[ i ] in duplicates ) {
+                        continue;
+                }
+                for( let j = i + 1; j < array.length; j++ ) {
+                        if( array[ i ] === array[ j ] ) {
+                                duplicates[ array[ i ] ] = j;
+                                break;
+                        }
+                }
         }
-        for( let j = i + 1; j < array.length; j++ ) {
-          if( array[ i ] === array[ j ] ) {
-            duplicates[ array[ i ] ] = j;
-            break;
-          }
-        }
-      }
     
       let firstDuplicate = sortDictionaryByValues( duplicates )[0] ;
       return firstDuplicate == null ? -1 : Number( firstDuplicate ) ;
-    }
+}
     
-    // Do not edit the line below.
-    exports.firstDuplicateValue = firstDuplicateValue;
+// Do not edit the line below.
+exports.firstDuplicateValue = firstDuplicateValue;
     
