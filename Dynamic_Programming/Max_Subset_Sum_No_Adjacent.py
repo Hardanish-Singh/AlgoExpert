9b744ce127019dfe6a9939fcb5750aa252045292
Hardanish Singh <hardanishsingh1@gmail.com>
2022-07-11 09:04:14 -0400

    Max Subset Sum No Adjacent

--- Dynamic_Programming/Max_Subset_Sum_No_Adjacent.py
+++ Dynamic_Programming/Max_Subset_Sum_No_Adjacent.py
@@ -1,3 +1,55 @@
+"""
+/****************************************************************
+*        ALGOEXPERT MAX SUBSET SUM NO ADJACENT CHALLENGE        *
+*                                                              	*
+* Problem Statement                                            	*
+* Write a function that takes in an array of positive integers  *
+* and returns the maximum sum of non-adjacent elements in the   *
+* array. If the array is empty, the function should return 0    *
+* 								*
+* Examples                                                     	*
+* Input 1: [75, 105, 120, 75, 90, 135]                          *
+* Output 1: 330                                           	*
+* 								*
+* Input 2: [2, 1, 5, 3, 3, 2, 4]                          	*
+* Output 2: 3                                           	*
+*                                                               *
+* Input 3: [1, 1, 2, 3, 3, 2, 2]                          	*
+* Output 3: 1                                           	*
+*                                                               *
+* Input 4: [3, 1, 3, 1, 1, 4, 4]                          	*
+* Output 4: 3                                           	*
+*                                                               *
+* Input 5: []                                             	*
+* Output 5: -1                                           	*
+*                                                               *
+* Input 6: [1]                                             	*
+* Output 6: -1                                           	*
+*                                                               *
+* Input 7: [1, 1]                                             	*
+* Output 7: -1                                           	*
+*                                                               *
+* Input 8: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10]                  *
+* Output 8: 10                                           	*
+*                                                               *
+* Input 9: [2, 1, 1]                                            *
+* Output 9: 1                                           	*
+*                                                               *
+* Input 10: [2, 2, 2, 2, 2, 2, 2, 2, 2]                         *
+* Output 10: 2                                           	*
+*                                                               *
+* Input 11: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]                     *
+* Output 11: -1                                           	*
+*                                                               *
+* Input 12: [7, 6, 5, 3, 6, 4, 3, 5, 2]                         *
+* Output 12: 6                                           	*
+*                                                               *
+* Input 13: [9, 13, 6, 2, 3, 5, 5, 5, 3, 2, 2, 2, 2, 4, 3]      *
+* Output 13: 5                                           	*
+*                                                               *
+****************************************************************/
+"""
+
 def maxSubsetSumNoAdjacent( array ):
         if len( array ) == 0:
                 return 0
