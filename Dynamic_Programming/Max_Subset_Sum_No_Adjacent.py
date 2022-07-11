d4e067db67dfe60d12cee52a549c77fdf0a83072
Hardanish Singh <hardanishsingh1@gmail.com>
2022-07-11 08:38:44 -0400

    Max Subset Sum No Adjacent

--- Dynamic_Programming/Max_Subset_Sum_No_Adjacent.py
+++ Dynamic_Programming/Max_Subset_Sum_No_Adjacent.py
@@ -3,3 +3,6 @@ def maxSubsetSumNoAdjacent( array ):
                 return 0
         if len( array ) == 1:
                 return array[ 0 ]
+        
+        dp = array
+        dp[ 1 ] = max( array[ 0 ], array[ 1 ] )
