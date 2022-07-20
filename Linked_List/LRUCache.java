90788cb1e76fa11790ba1281b0cdcbea69174a51
Hardanish Singh <hardanishsingh1@gmail.com>
2022-07-20 09:44:45 -0400

    LRU Cache

--- Linked_List/LRUCache.java
+++ Linked_List/LRUCache.java
@@ -53,3 +53,106 @@ class Node
                 this.previous.next = node;
         }
 }
+
+
+class Program 
+{
+  
+        static class LRUCache 
+        {
+                // int maxSize;
+
+                private int capacity;
+                private Node head = null;
+                private Node tail = null;
+                // HashMap is a key-value pair of Keys as "Key Number" & Value as "Node Address"
+                private HashMap<String, Node> map = null;
+
+                public LRUCache(int capacity) 
+                {
+                        // this.maxSize = maxSize > 1 ? maxSize : 1;
+                        // Initialize Capacity
+                        this.capacity = capacity;
+                        // Initialize Doubly Linked List
+                        head = new Node( "", 0 );
+                        tail = new Node( "", 0 );
+                        head.setNextNode( tail );
+                        tail.setPreviousNode( head );
+                        head.setPreviousNode( null );
+                        tail.setNextNode( null );
+                        // Initialize HashMap
+                        map = new HashMap<String, Node>();
+                }
+
+                public void insertKeyValuePair(String key, int value) 
+                {
+                        if( map.containsKey( key ) )
+                        {
+                                // Remove the Node from the Doubly Linked List
+                                Node node = map.get( key );
+                                delete( node );
+                        }
+                        if( map.size() == capacity )
+                        {
+                                // Remove the Node before Tail Node from the Doubly Linked List
+                                Node node = map.get( tail.getPreviousNode().getKey() );
+                                delete( node );
+                        }
+                        Node node = new Node( key, value );
+                        insert( node );
+                }
+
+                public LRUResult getValueFromKey(String key) 
+                {
+                        if( map.containsKey( key ) )
+                        {
+                                Node node = map.get( key );
+                                delete( node );
+                                insert( node );
+                                return new LRUResult( true, node.getValue() );
+                        }
+                        else
+                        {
+                                return null;
+                        }
+                }
+
+                public String getMostRecentKey() 
+                {
+                        return head.getNextNode().getKey();
+                }
+
+                public void insert( Node node )
+                {
+                        // Add the key in HashMap
+                        map.put( node.getKey(), node );
+                        // Add the node at the first position always
+                        node.setNextNode( head.getNextNode() );
+                        node.setNextAndPreviousNode( node );
+                        head.setNextNode( node );
+                        node.setPreviousNode( head );
+                }
+
+                public void delete( Node node )
+                {
+                        // Remove the key from the HashMap
+                        map.remove( node.getKey() );
+                        // Remove the node from the Doubly Linked List
+                        node.setPreviousAndNextNode( node.getNextNode() );
+                        node.setNextAndPreviousNode( node.getPreviousNode() );
+                }
+        }
+
+  static class LRUResult 
+  {
+    boolean found;
+    int value;
+
+    public LRUResult(boolean found, int value) 
+    {
+      this.found = found;
+      this.value = value;
+    }
+  }
+
+}
