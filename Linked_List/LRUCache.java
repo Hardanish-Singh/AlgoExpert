8f80cfc581794949a819c7141fa360da03ca90f6
Hardanish Singh <hardanishsingh1@gmail.com>
2022-07-20 09:46:18 -0400

    LRU Cache

--- Linked_List/LRUCache.java
+++ Linked_List/LRUCache.java
@@ -145,7 +145,7 @@ class Program
                 boolean found;
                 int value;
 
-                public LRUResult(boolean found, int value) 
+                public LRUResult( boolean found, int value ) 
                 {
                         this.found = found;
                         this.value = value;
