c38283e943c2cde54c4cb8635c9e2e8bd19b8660
Hardanish Singh <hardanishsingh1@gmail.com>
2022-07-20 10:28:35 -0400

    LRU Cache

--- Linked_List/LRUCache.java
+++ Linked_List/LRUCache.java
@@ -49,21 +49,9 @@
                         { "arguments": ["b", 2], "method": "insertKeyValuePair", "output": null },
                         { "arguments": ["a", 1], "method": "insertKeyValuePair", "output": null },
                         { "arguments": ["c", 3], "method": "insertKeyValuePair", "output": null },
-                        {
-                                "arguments": [],
-                                "method": "getMostRecentKey",
-                                "output": "c"
-                        },
-                        {
-                                "arguments": ["a"],
-                                "method": "getValueFromKey",
-                                "output": 1
-                        },
-                        {
-                                "arguments": [],
-                                "method": "getMostRecentKey",
-                                "output": "a"
-                        },
+                        { "arguments": [], "method": "getMostRecentKey", "output": "c" },
+                        { "arguments": ["a"], "method": "getValueFromKey", "output": 1 },
+                        { "arguments": [], "method": "getMostRecentKey", "output": "a" },
                         {
                                 "arguments": ["d", 4],
                                 "method": "insertKeyValuePair",
