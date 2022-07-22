b419bf2355f4810de9cd55d1fd6e2e5ecf34e411
Hardanish Singh <hardanishsingh1@gmail.com>
2022-07-22 13:04:04 -0400

    Node Swap

--- Linked_List/Node_Swap.java
+++ Linked_List/Node_Swap.java
@@ -1,3 +1,28 @@
+/****************************************************************
+*             	ALGOEXPERT NODE SWAP CHALLENGE	        	*
+*                                                              	*
+* Write a function that takes in the head of a Singly Linked 	*
+* List, swaps every pair of adjacent nodes in place (i.e., 	*
+* doesn't create a brand new list), and returns its new head. 	*
+*                                                               *
+* If the input Linked List has an odd number of nodes, its final*
+* node should remain the same.					*
+*								*
+* Each LinkedList node has an integer value as well as a next 	*
+* node pointing to the next node in the list or to  None / null *
+* if it's the tail of the list.					*
+*								*
+* You can assume that the input Linked List will always have at *
+* least one node; in other words, the head will never be 	*
+* None / null.							*
+*								*
+* Input 1							*
+* 		head = 0 -> 1 -> 2 -> 3 -> 4 -> 5 		*
+* Output 1							*
+*		1 -> 0 -> 3 -> 2 -> 5 -> 4 			*
+*								*
+****************************************************************/
+
 import java.util.*;
 
 class Program
