3ae1a2cfcef638375c0874a7e72041af3e218179
Hardanish Singh <hardanishsingh1@gmail.com>
2022-07-25 12:43:40 -0400

    Zip Linked List

--- Linked_List/Zip_Linked_List.java
+++ Linked_List/Zip_Linked_List.java
@@ -11,35 +11,35 @@ class Program
                 }
         }
 
-  public static LinkedList middleNode(LinkedList head) 
-  {
-          double middle = 0;
-          LinkedList current = head;
-                  
-          while( current != null )
-          {
-                  middle++;
-                  current = current.next;
-          }
-          if( middle % 2 == 0 )
-          {
-                  middle++;
-          }
-          middle = Math.ceil( middle / 2 );
-          
-          double count = 1;
-          while( head != null )
-          {
-                  if( count == middle ) 
-                  {
-                          break;
-                  }
-                  head = head.next;
-                  count++;
-          }
-          
-          return head;
-  }
+        public static LinkedList middleNode(LinkedList head) 
+        {
+                double middle = 0;
+                LinkedList current = head;
+
+                while( current != null )
+                {
+                        middle++;
+                        current = current.next;
+                }
+                if( middle % 2 == 0 )
+                {
+                        middle++;
+                }
+                middle = Math.ceil( middle / 2 );
+
+                double count = 1;
+                while( head != null )
+                {
+                        if( count == middle ) 
+                        {
+                                break;
+                        }
+                        head = head.next;
+                        count++;
+                }
+
+                return head;
+        }
 
   public static LinkedList reverseList( LinkedList head )
         {
